f64f6b3168ace991fb84f10001a0d06b02738045
a900649 <53169241+a900649@users.noreply.github.com>
2022-12-02 15:28:24 +0800

    Add files via upload

--- electricity_statistics.py
+++ electricity_statistics.py
@@ -171,11 +171,11 @@ def show_path():
     st.header(" , ".join(os.listdir()))
     st.header(" , ".join(os.listdir("用電統計資料")))
 
-    filename = os.getcwd() + "\\用電統計資料\\{}.csv".format(str(104))
+    filename = "用電統計資料\\104.csv"
     file_exist = os.path.isfile(filename)
     st.header(str(file_exist))
 
-    filename = os.getcwd() + "\\" + "用電統計暫存資料\\用電統計.pkl"
+    filename = "用電統計暫存資料\\用電統計.pkl"
     file_exist = os.path.isfile(filename)
     st.header(str(file_exist))
 

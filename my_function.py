1815334ccf5e8bb89e920cdcddcadbc6d995d725
a900649 <53169241+a900649@users.noreply.github.com>
2022-12-02 15:05:46 +0800

    Add files via upload

--- my_function.py
+++ my_function.py
@@ -1,19 +1,14 @@
 import pickle
-import os
 
 def save_obj(obj, name ):
 
-    a = os.path.dirname(os.path.abspath(__file__))
-
-    with open(a + "\\" + name + '.pkl', 'wb') as f:
+    with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name ):
-    a = os.path.dirname(os.path.abspath(__file__))
-
     if ".pkl" in name :
         name = name
     else :
         name = name + '.pkl'
-    with open(a + "\\" +name , 'rb') as f:
+    with open(name , 'rb') as f:
         return pickle.load(f)
